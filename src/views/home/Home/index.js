90b7604f6fb13a8c5f4fe8466cd830c330b231a7
jdbwin <jdbldwin@gmail.com>
2018-11-14 21:47:12 -0500

    setup cards

--- src/views/home/Home/index.js
+++ src/views/home/Home/index.js
@@ -2,11 +2,25 @@ import React from 'react'
 
 import Search from '../../../containers/Words/Search'
 import Hero from '../Hero'
+import Card from '../../../components/Card'
 
 const Home = () => (
   <div className="column is-full">
     <Hero />
     <Search />
+    <div className="container m-t-xl">
+      <div className="columns">
+        <div className="column">
+          <Card title="Top" />
+        </div>
+        <div className="column">
+          <Card title="Random" />
+        </div>
+        <div className="column">
+          <Card title="Recent" />
+        </div>
+      </div>
+    </div>
   </div>
 )
 
